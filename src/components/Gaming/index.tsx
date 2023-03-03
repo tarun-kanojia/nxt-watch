8e41266319b7061c835184f165d8e6c6e6e7b2e0
tarun-kanojia <tarunkanojiawork@gmail.com>
2023-03-03 10:24:36 +0530

    intial developement branch commit

--- src/components/Gaming/index.tsx
+++ src/components/Gaming/index.tsx
@@ -1,32 +1,24 @@
 import { inject, Observer, observer } from 'mobx-react';
 import React, { useEffect, useState } from 'react'
 import { FaGamepad } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import { GAMING_VIDEO_URL } from '../../constants/endPoints';
 import { APIStatus } from '../../constants/errorStatus';
 import { GamingVideoList } from '../../model/GamingVideoList';
-import { GamingVideoListResponse, VideoBaseResponse, VideoListResponse } from '../../model/types';
 import { VideoBase } from '../../model/VideoBase';
-import { VideoList } from '../../model/VideoList';
-import { gamingVideoListStore } from '../../store/gamingVideoListStore';
 import { GamingVideoStore } from '../../store/GamingVideoStore';
 import { LOCAL_STORAGE } from '../../util/storage/constant';
 import { getCookie } from '../../util/storage/StorageUtil';
-import { getVideoListFromStore, updateVideoListToStore } from '../../util/storage/VideoListStore';
-import ErrorComponent from '../ErrorComponent';
 import { Render } from '../Home';
 import { PageWrapper } from '../Home/style';
-import Loader from '../Loader';
 import { StyledLink } from '../Login/style';
 import PageHeader from '../PageHeader';
-import { GamingContainer, GamingVideoCardWrapper, GamingVideosContainer, GamingVideoThumbnail, LiveWatching, VideoDescription, VideoTitle } from './style';
+import { GamingVideoCardWrapper, GamingVideosContainer, GamingVideoThumbnail, LiveWatching, VideoDescription, VideoTitle } from './style';
 
 interface GamingInjectedProps {
     gamingVideoStore: GamingVideoStore
 }
 
 const Gaming = inject('gamingVideoStore')(
-    observer((props:any) => {
+    observer((props: any) => {
         const [errorStatus, setErrorStatus] = useState(APIStatus.IN_PROGRESS)
         const [videoDataList, setVideoDataList] = useState<GamingVideoList>()
         const [gamingVideoList, setGamingVideoList] = useState<VideoBase[] | null>(null)
@@ -40,7 +32,7 @@ const Gaming = inject('gamingVideoStore')(
         const getVideoList = async () => {
             try {
 
-                if(!gamingVideoStore.videos.length){
+                if (!gamingVideoStore.videos.length) {
                     const bearerToken = getCookie(LOCAL_STORAGE.JWT_TOKEN)
                     gamingVideoStore.loadVideos(bearerToken);
                     // console.log(gamingVideoStore);
--- src/components/Login/ValidationUtil.ts
+++ src/components/Login/ValidationUtil.ts
@@ -1,67 +1,68 @@
-import { LoginMetaData } from "../../model/LoginMetaData"
-import { ErrorModel } from "../../model/types"
+import { LoginMetaData } from "../../model/LoginMetaData";
+import { ErrorModel } from "../../model/types";
 
-const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
-
+const passwordRegex =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 
 const validateUserPassword = (userPassword: string) => {
-    const userPasswordValidation = userPassword.match(passwordRegex)
+    const userPasswordValidation = userPassword.match(passwordRegex);
 
     return new Promise((resolve, reject) => {
-        const regExResult = userPasswordValidation
-        if (userPassword.length == 0) reject({ valid: false, errorName: 'EMPTY_PASSWORD' })
+        const regExResult = userPasswordValidation;
+        if (userPassword.length == 0)
+            reject({ valid: false, errorName: "EMPTY_PASSWORD" });
         else if (regExResult === null || regExResult.length == 0) {
-            console.log('password Rejected')
+            console.log("password Rejected");
             reject({
                 valid: false,
-                errorName: 'INVALID_PASSWORD'
-            })
-
+                errorName: "INVALID_PASSWORD",
+            });
         } else {
-            resolve({ valid: true })
+            resolve({ valid: true });
         }
-
-    })
-}
+    });
+};
 
 const validateUserName = (userNameInput: string) => {
-    const userNameValidation = new RegExp("(^\d|[\$\&\+\,\:\;\=\?\@\#\|\'\<\>\.\^\*\(\)\%\!\'\-\'\'])", "g");
+    const userNameValidation = new RegExp(
+        "(^d|[$&+,:;=?@#|'<>.^*()%!'-''])",
+        "g"
+    );
     return new Promise((resolve, reject) => {
         // console.log('RegEx output: ', userNameValidation.exec(userNameInput))
 
         if (userNameInput.length == 0) {
-            console.log('username Rejected');
+            console.log("username Rejected");
             reject({
                 valid: false,
-                errorName: 'EMPTY_USERNAME'
-            })
-        }
-        else if (userNameValidation.exec(userNameInput) !== null) {
+                errorName: "EMPTY_USERNAME",
+            });
+        } else if (userNameValidation.exec(userNameInput) !== null) {
             console.log(userNameValidation.exec(userNameInput));
             reject({
                 valid: false,
-                errorName: 'INVALID_USERNAME'
-            })
-
+                errorName: "INVALID_USERNAME",
+            });
         } else {
-            resolve({ valid: true })
+            resolve({ valid: true });
         }
-    })
-}
+    });
+};
 
 export const validateForm = (loginMetaData: LoginMetaData) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const usernameValidation = await validateUserName(loginMetaData.username);
-            const userPasswordValidation = await validateUserPassword(loginMetaData.password);
+            const usernameValidation = await validateUserName(
+                loginMetaData.username
+            );
+            const userPasswordValidation = await validateUserPassword(
+                loginMetaData.password
+            );
 
-            resolve({ valid: true })
-        }
-
-        catch (error:any) {
-            reject({valid:false, value:error.errorName.toString()})
+            resolve({ valid: true });
+        } catch (error: any) {
+            reject({ valid: false, value: error.errorName.toString() });
             // throw new Error();
         }
-
     });
-}
+};
