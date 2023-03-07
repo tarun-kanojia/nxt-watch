4d13685c1e0602666639f4db7be0b8f913087681
tarun-kanojia <tarunkanojiawork@gmail.com>
2023-03-07 11:59:10 +0530

    completed unit testing for video card

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
-import React, { useState } from 'react';
-import './App.css';
-import { BrowserRouter, Navigate, Route, Routes, useNavigate } from 'react-router-dom';
-import Login from './components/Login';
-import ProtectedRoute from './components/ProtectedRoute';
-import NotFound from './components/NotFound';
-import { GlobalStyle } from './components/Login/style';
-import NxtWatch from './components/NxtWatch';
-import { ROUTES } from './components/Routes/constants';
-import { ThemeProvider } from 'styled-components';
-import { ThemeContextHook } from './hooks/ThemeContext';
-import { getCookie } from './util/storage/StorageUtil';
-import { JWTTokenContext } from './hooks/JWTTokenContext';
-import { Video } from './model/Video';
-import { SavedVideosContext } from './hooks/SavedVideos';
-import { LogoutModalContext } from './hooks/ModalContext';
-import { LOCAL_STORAGE } from './util/storage/constant';
-import { THEME_DARK, THEME_LIGHT } from './constants/style';
-import { RootStore } from './store/RootStore';
-import { TransportLayer } from './service/TarnsportLayer/index.api';
-import { GamingVideoStore } from './store/GamingVideoStore';
-import { HomeVideoStore } from './store/HomeVideoStore';
-import { TrendingVideoStore } from './store/TrendingVideoStore';
-import { Provider } from 'mobx-react';
-import { SavedVideosStore } from './store/SavedVideosStore';
+import React, { useState } from "react";
+import "./App.css";
+import {
+    BrowserRouter,
+    Navigate,
+    Route,
+    Routes,
+    useNavigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import NotFound from "./components/NotFound";
+import { GlobalStyle } from "./components/Login/style";
+import NxtWatch from "./components/NxtWatch";
+import { ROUTES } from "./components/Routes/constants";
+import { ThemeProvider } from "styled-components";
+import { ThemeContextHook } from "./hooks/ThemeContext";
+import { getCookie } from "./util/storage/StorageUtil";
+import { JWTTokenContext } from "./hooks/JWTTokenContext";
+import { Video } from "./model/Video";
+import { SavedVideosContext } from "./hooks/SavedVideos";
+import { LogoutModalContext } from "./hooks/ModalContext";
+import { LOCAL_STORAGE } from "./util/storage/constant";
+import { THEME_DARK, THEME_LIGHT } from "./constants/style";
+import { RootStore } from "./store/RootStore";
+import { TransportLayer } from "./service/TarnsportLayer/index.api";
+import { GamingVideoStore } from "./store/GamingVideoStore";
+import { HomeVideoStore } from "./store/HomeVideoStore";
+import { TrendingVideoStore } from "./store/TrendingVideoStore";
+import { Provider } from "mobx-react";
+import { SavedVideosStore } from "./store/SavedVideosStore";
 
 const rootStore = new RootStore(null);
 const transportLayer = new TransportLayer(null);
@@ -35,88 +41,120 @@ rootStore.updateGamingVideoRef(gamingVideoStore);
 rootStore.updateTrendingVideoRef(trendingVideoStore);
 rootStore.updateSavedVideoRef(savedVideoStore);
 
+function App() {
+    const [theme, setTheme] = useState("light");
+    const [jwtToken, setJwtToken] = useState(
+        getCookie(LOCAL_STORAGE.JWT_TOKEN)
+    );
+    const [savedVideos, setSavedVideos] = useState<Video[]>([]);
+    const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
 
-function App () {
-  const [theme, setTheme] = useState('light')
-  const [jwtToken, setJwtToken] = useState(getCookie(LOCAL_STORAGE.JWT_TOKEN))
-  const [savedVideos, setSavedVideos] = useState<Video[]>([])
-  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+    const updateSaveVideoList = (data: Video) => {
+        let newSavedVideoList = [...savedVideos];
+        if (!newSavedVideoList.some((video) => video.id === data.id)) {
+            newSavedVideoList.push(data);
+        } else {
+            newSavedVideoList = newSavedVideoList.filter(
+                (video) => video.id !== data.id
+            );
+        }
+        // console.log(newSavedVideoList);
+        setSavedVideos(newSavedVideoList);
+    };
 
-  const updateSaveVideoList = (data: Video) => {
-    let newSavedVideoList = [...savedVideos];
-    if (!newSavedVideoList.some((video) => video.id === data.id)) {
-      newSavedVideoList.push(data)
-    } else {
-      newSavedVideoList = newSavedVideoList.filter((video) => video.id !== data.id)
-    }
-    // console.log(newSavedVideoList);
-    setSavedVideos(newSavedVideoList);
-  }
+    const closeLogoutModal = () => {
+        // console.log('modal close')
+        setIsLogoutModalOpen(false);
+    };
 
+    const openLogoutModal = () => {
+        // console.log('modal open')
+        setIsLogoutModalOpen(true);
+    };
+    const toggleModal = () => {
+        setIsLogoutModalOpen(!isLogoutModalOpen);
+    };
 
-  const closeLogoutModal = () => {
-    // console.log('modal close')
-    setIsLogoutModalOpen(false);
-  }
+    const setToken = (token: string) => {
+        setJwtToken(token);
+    };
 
-  const openLogoutModal = () => {
-    // console.log('modal open')
-    setIsLogoutModalOpen(true);
-  }
-  const toggleModal = () => {
-    setIsLogoutModalOpen(!isLogoutModalOpen);
-  }
+    return (
+        <BrowserRouter>
+            <Provider transportLayerRef={transportLayer} rootStore={rootStore}>
+                <JWTTokenContext.Provider value={{ jwtToken, setToken }}>
+                    <LogoutModalContext.Provider
+                        value={{
+                            isLogoutModalOpen,
+                            openLogoutModal,
+                            closeLogoutModal,
+                            toggleModal,
+                        }}
+                    >
+                        <SavedVideosContext.Provider
+                            value={{ savedVideos, updateSaveVideoList }}
+                        >
+                            <ThemeContextHook.Provider
+                                value={{ active: theme, toggleTheme: setTheme }}
+                            >
+                                <ThemeProvider
+                                    theme={
+                                        theme === "light"
+                                            ? THEME_LIGHT
+                                            : THEME_DARK
+                                    }
+                                >
+                                    <GlobalStyle />
 
-  const setToken = (token: string) => {
-    setJwtToken(token);
-  }
+                                    <Routes>
+                                        <Route
+                                            path="/login"
+                                            element={<Login />}
+                                        />
+                                        {ROUTES.map((route) => {
+                                            const Element = route.element;
 
+                                            return (
+                                                <Route
+                                                    key={route.path}
+                                                    path={route.path}
+                                                    element={
+                                                        <ProtectedRoute
+                                                            renderElement={() => (
+                                                                <NxtWatch
+                                                                    children={
+                                                                        <Element />
+                                                                    }
+                                                                />
+                                                            )}
+                                                        />
+                                                    }
+                                                />
+                                            );
+                                        })}
 
-  return (
-
-    <BrowserRouter>
-      <Provider transportLayerRef={transportLayer} rootStore={rootStore}>
-
-        <JWTTokenContext.Provider value={{ jwtToken, setToken }}>
-          <LogoutModalContext.Provider value={{ isLogoutModalOpen, openLogoutModal, closeLogoutModal, toggleModal }}>
-
-
-            <SavedVideosContext.Provider value={{ savedVideos, updateSaveVideoList }}>
-
-
-              <ThemeContextHook.Provider value={{ active: theme, toggleTheme: setTheme }}>
-
-                <ThemeProvider theme={theme === 'light' ? THEME_LIGHT : THEME_DARK}>
-
-                  <GlobalStyle />
-
-
-                  <Routes>
-
-                    <Route path='/login' element={<Login />} />
-                    {ROUTES.map((route) => {
-                      const Element = route.element;
-
-                      return (
-                        <Route key={route.path} path={route.path} element={
-                          <ProtectedRoute renderElement={() => <NxtWatch children={<Element />} />} />
-                        } />
-                      )
-                    })}
-
-
-                    <Route path='/not-found' element={<NotFound />} />
-                    <Route path='*' element={<Navigate to='/not-found' replace />} />
-
-                  </Routes>
-                </ThemeProvider>
-              </ThemeContextHook.Provider>
-            </SavedVideosContext.Provider>
-          </LogoutModalContext.Provider>
-        </JWTTokenContext.Provider>
-      </Provider>
-    </BrowserRouter>
-  );
+                                        <Route
+                                            path="/not-found"
+                                            element={<NotFound />}
+                                        />
+                                        <Route
+                                            path="*"
+                                            element={
+                                                <Navigate
+                                                    to="/not-found"
+                                                    replace
+                                                />
+                                            }
+                                        />
+                                    </Routes>
+                                </ThemeProvider>
+                            </ThemeContextHook.Provider>
+                        </SavedVideosContext.Provider>
+                    </LogoutModalContext.Provider>
+                </JWTTokenContext.Provider>
+            </Provider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/VideoComponent/index.tsx
+++ src/components/VideoComponent/index.tsx
@@ -1,40 +1,59 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
-import { VIDEOS_BASE_URL, VIDEO_URL } from '../../constants/endPoints';
-import { VideoResponse } from '../../model/types';
-import { Video } from '../../model/Video';
-import { getDuration } from '../../util/DateFunction';
-import { getCookie } from '../../util/storage/StorageUtil';
-import { ActionButtonWrapper, CenterContainer, ChannelDescription, ChannelDetails, ChannelName, ChannelProfile, ChannelSubscribersCount, ChannelWrapper, Divider, DoteIcon, Duration, Title, VideoActonWrapper, VideoAnalyticsWrapper, VideoContainer, ViewCount, YoutubeEmbed } from './style';
-import { BiLike, BiDislike, BiSave } from 'react-icons/bi'
-import ActionIconButton from '../ActionIconButton';
-import { ActionIconButtonList } from '../../model/ActionIconButton';
-import { ICONS } from '../ActionIconButton/constant';
-import ActionButtonList from '../ActionButtonsList';
-import { SavedVideosContext, SavedVideosType } from '../../hooks/SavedVideos';
-import { LOCAL_STORAGE } from '../../util/storage/constant';
-import { PageWrapper } from '../Home/style';
-import Loader from '../Loader';
-import { LikedStatus, LikedStatusType } from '../../constants/errorStatus';
-import { addVideoDataToStore, getVideoDataFromStore, videoComponentListStore } from '../../store/videoComponentStore';
-import { inject, observer } from 'mobx-react';
-import { SavedVideosStore } from '../../store/SavedVideosStore';
-import { reaction, autorun } from 'mobx'
-interface VideoComponentProps {
-
-}
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { VIDEOS_BASE_URL, VIDEO_URL } from "../../constants/endPoints";
+import { VideoResponse } from "../../model/types";
+import { Video } from "../../model/Video";
+import { getDuration } from "../../util/DateFunction";
+import { getCookie } from "../../util/storage/StorageUtil";
+import {
+    ActionButtonWrapper,
+    CenterContainer,
+    ChannelDescription,
+    ChannelDetails,
+    ChannelName,
+    ChannelProfile,
+    ChannelSubscribersCount,
+    ChannelWrapper,
+    Divider,
+    DoteIcon,
+    Duration,
+    Title,
+    VideoActonWrapper,
+    VideoAnalyticsWrapper,
+    VideoContainer,
+    ViewCount,
+    YoutubeEmbed,
+} from "./style";
+import { BiLike, BiDislike, BiSave } from "react-icons/bi";
+import ActionIconButton from "../ActionIconButton";
+import { ActionIconButtonList } from "../../model/ActionIconButton";
+import { ICONS } from "../ActionIconButton/constant";
+import ActionButtonList from "../ActionButtonsList";
+import { SavedVideosContext, SavedVideosType } from "../../hooks/SavedVideos";
+import { LOCAL_STORAGE } from "../../util/storage/constant";
+import { PageWrapper } from "../Home/style";
+import Loader from "../Loader";
+import { LikedStatus, LikedStatusType } from "../../constants/errorStatus";
+import {
+    addVideoDataToStore,
+    getVideoDataFromStore,
+    videoComponentListStore,
+} from "../../store/videoComponentStore";
+import { inject, observer } from "mobx-react";
+import { SavedVideosStore } from "../../store/SavedVideosStore";
+import { reaction, autorun } from "mobx";
+interface VideoComponentProps {}
 
 interface VideoComponentInjectedProps {
     savedVideoStore: SavedVideosStore;
 }
 
 export const getVideoResponseTypeData = (data: Video) => {
-    return ({
+    return {
         channel: {
             name: data.channel.name,
             profile_image_url: data.channel.profileImageUrl,
-            subscriber_count: data.channel.subscriberCount
-
+            subscriber_count: data.channel.subscriberCount,
         },
         id: data.id,
         published_at: data.publishedAt,
@@ -43,73 +62,64 @@ export const getVideoResponseTypeData = (data: Video) => {
         view_count: data.viewCount,
         video_url: data.videoUrl,
         is_liked: data.isLiked,
-        description: data.description
-    })
-}
-
+        description: data.description,
+    };
+};
 
 export const youtubeStyleOpts = {
-    height: '100%',
-    width: '100%',
+    height: "100%",
+    width: "100%",
     playerVars: {
         // https://developers.google.com/youtube/player_parameters
         autoplay: 0,
     },
 };
 
-const savedVideoStatus = (videoList: SavedVideosType | null, id: string | undefined) => {
-    const isSaved = videoList ?
-        videoList.savedVideos.some((video) => video.id == id)
+const savedVideoStatus = (
+    videoList: SavedVideosType | null,
+    id: string | undefined
+) => {
+    const isSaved = videoList
+        ? videoList.savedVideos.some((video) => video.id == id)
         : false;
     return isSaved;
-}
+};
 
-
-
-
-const VideoComponent = inject('savedVideoStore')(
+const VideoComponent = inject("savedVideoStore")(
     observer((props: any) => {
         const videoId = useParams().id;
         const videoList = useContext(SavedVideosContext);
-        const [videoData, setVideoData] = useState<Video | null>(null)
-        const [actionIconButtonList, setActionIconButtonList] = useState(new ActionIconButtonList(ICONS))
-        const [videoSavedStatus, setVideoSavedStatus] = useState(savedVideoStatus(videoList, videoId));
+        const [videoData, setVideoData] = useState<Video | null>(null);
+        const [actionIconButtonList, setActionIconButtonList] = useState(
+            new ActionIconButtonList(ICONS)
+        );
+        const [videoSavedStatus, setVideoSavedStatus] = useState(
+            savedVideoStatus(videoList, videoId)
+        );
 
         const { savedVideoStore } = props as VideoComponentInjectedProps;
 
-        // autorun(() => {
-        //     console.log('storeLen changes: ', savedVideoStore.videos.length);
-        // })
-
-        // reaction(() => savedVideoStore.videos.length,
-        //     len => {
-        //         console.log(len);
-        //         console.log('video saved')
-        //     },
-        //     { fireImmediately: true }
-        // )
-
         const updateVideoSavedStatus = () => {
             setVideoSavedStatus(!videoSavedStatus);
-
-        }
+        };
 
         const updateActionButtonList = (list: ActionIconButtonList) => {
-            setActionIconButtonList(new ActionIconButtonList(list))
-        }
+            setActionIconButtonList(new ActionIconButtonList(list));
+        };
 
         //extracting id from url
         const getVideoId = (videoUrl: string | undefined) => {
-            return videoUrl == undefined ? ''
-                : videoUrl.substring(videoUrl.indexOf('=') + 1);
-        }
+            return videoUrl == undefined
+                ? ""
+                : videoUrl.substring(videoUrl.indexOf("=") + 1);
+        };
 
         const updateVideoData = (data: Video) => {
             // const newData = getVideoResponseTypeData({...data});
             // console.log(newData)
             setVideoData(data);
             // console.log(newData.isLiked)
-        }
+        };
 
         const updateTheStatus = (newVideoData: Video) => {
             if (videoList) {
@@ -117,22 +127,16 @@ const VideoComponent = inject('savedVideoStore')(
                 updateVideoData(newVideoData);
                 // console.log('inside updateTheStatus: ', videoList)
                 updateVideoSavedStatus();
-
             }
-
-        }
+        };
 
         const toggleSavedStatus = () => {
-
             if (videoData) {
                 videoData.toggleSavedStatus();
-                updateTheStatus(videoData)
+                updateTheStatus(videoData);
                 updateVideoSavedStatus();
-
-
             }
-
-        }
+        };
 
         const getVideoData = async () => {
             const jwtToken = getCookie(LOCAL_STORAGE.JWT_TOKEN);
@@ -140,115 +144,114 @@ const VideoComponent = inject('savedVideoStore')(
             try {
                 if (!videoId) throw new Error();
 
-
                 const requestOption = {
                     headers: {
                         Authorization: `Bearer ${jwtToken}`,
-                        "Content-Type": "application/json"
+                        "Content-Type": "application/json",
                     },
 
-                    method: 'GET'
-
-                }
+                    method: "GET",
+                };
                 let videoResponseData: Video | null = null;
                 const storeData = getVideoDataFromStore(videoId);
                 if (storeData === null) {
-
-                    const response = await fetch(VIDEOS_BASE_URL + `/${videoId}`, requestOption);
-                    const responseData: { video_details: VideoResponse } = await response.json();
+                    const response = await fetch(
+                        VIDEOS_BASE_URL + `/${videoId}`,
+                        requestOption
+                    );
+                    const responseData: { video_details: VideoResponse } =
+                        await response.json();
                     // console.log(responseData)
-                    const videoResponse: VideoResponse = responseData.video_details;
+                    const videoResponse: VideoResponse =
+                        responseData.video_details;
                     videoResponseData = new Video(videoResponse);
                     // setVideoData(new Video(responseVideoData));
 
                     addVideoDataToStore(videoResponseData);
-
                 } else {
-                    videoResponseData = getVideoDataFromStore(videoId)
-
+                    videoResponseData = getVideoDataFromStore(videoId);
                 }
+
                 window.setTimeout(() => {
-                    if (videoResponseData !== null) updateVideoData(videoResponseData)
+                    if (videoResponseData !== null)
+                        updateVideoData(videoResponseData);
                 }, 100);
-
-
-
-
             } catch (error) {
                 console.log(error);
             }
-        }
+        };
         // console.log(videoData?.isLiked);
-
 
         useEffect(() => {
             getVideoData();
-
-
-        }, [])
-
+        }, []);
 
         const onClickIcon = (videoData: Video) => {
             if (videoList !== null) {
-                videoList.updateSaveVideoList(videoData)
+                videoList.updateSaveVideoList(videoData);
             }
-
-        }
+        };
         // console.log(videoSavedStatus)
 
-        return (
-            videoData == null ?
-                <Loader />
-
-                :
-                <PageWrapper>
-
-                    <VideoContainer>
-
-                        <YoutubeEmbed videoId={getVideoId(videoData.videoUrl)} opts={youtubeStyleOpts} />
-                        <Title>{videoData.title}</Title>
-                        <VideoActonWrapper>
-                            <VideoAnalyticsWrapper>
-                                <ViewCount>{`${videoData.viewCount} views`}</ViewCount>
-                                <DoteIcon />
-                                <Duration>{`${getDuration(videoData.publishedAt)} ago`}</Duration>
-                            </VideoAnalyticsWrapper>
-                            <ActionButtonWrapper>
-                                <ActionButtonList
-                                    actionIconButtonList={actionIconButtonList}
-                                    updateActionButtonList={updateActionButtonList}
-                                    video={videoData}
-                                    updateVideoData={updateVideoData}
-                                />
-                                <CenterContainer onClick={() => {
-
+        return videoData == null ? (
+            <Loader />
+        ) : (
+            <PageWrapper data-testid="full-video-card">
+                <VideoContainer>
+                    <YoutubeEmbed
+                        videoId={getVideoId(videoData.videoUrl)}
+                        opts={youtubeStyleOpts}
+                    />
+                    <Title>{videoData.title}</Title>
+                    <VideoActonWrapper>
+                        <VideoAnalyticsWrapper>
+                            <ViewCount>{`${videoData.viewCount} views`}</ViewCount>
+                            <DoteIcon />
+                            <Duration>{`${getDuration(
+                                videoData.publishedAt
+                            )} ago`}</Duration>
+                        </VideoAnalyticsWrapper>
+                        <ActionButtonWrapper>
+                            <ActionButtonList
+                                actionIconButtonList={actionIconButtonList}
+                                updateActionButtonList={updateActionButtonList}
+                                video={videoData}
+                                updateVideoData={updateVideoData}
+                            />
+                            <CenterContainer
+                                onClick={() => {
                                     toggleSavedStatus();
                                     savedVideoStore.toggleVideoData(videoData);
                                     // updateVideoSavedStatus();
-                                }}>
+                                }}
+                            >
+                                <BiSave
+                                    size="2rem"
+                                    color={
+                                        videoSavedStatus ? "#3b82f6" : "grey"
+                                    }
+                                />
+                                <span>Save</span>
+                            </CenterContainer>
+                        </ActionButtonWrapper>
+                    </VideoActonWrapper>
 
-                                    <BiSave size='2rem'
-                                        color={videoSavedStatus ? '#3b82f6' : 'grey'}
-                                    />
-                                    <span>Save</span>
-                                </CenterContainer>
-                            </ActionButtonWrapper>
-                        </VideoActonWrapper>
+                    <Divider />
 
-                        <Divider />
-
-                        <ChannelWrapper>
-                            <ChannelProfile src={videoData.channel.profileImageUrl} />
-                            <ChannelDetails>
-                                <ChannelName>{videoData.channel.name}</ChannelName>
-                                <ChannelSubscribersCount>{`${videoData.channel.subscriberCount} subscribers`}</ChannelSubscribersCount>
-                                <ChannelDescription>{videoData.description}</ChannelDescription>
-                            </ChannelDetails>
-                        </ChannelWrapper>
-
-                    </VideoContainer>
-                </PageWrapper>
-
+                    <ChannelWrapper>
+                        <ChannelProfile
+                            src={videoData.channel.profileImageUrl}
+                        />
+                        <ChannelDetails>
+                            <ChannelName>{videoData.channel.name}</ChannelName>
+                            <ChannelSubscribersCount>{`${videoData.channel.subscriberCount} subscribers`}</ChannelSubscribersCount>
+                            <ChannelDescription>
+                                {videoData.description}
+                            </ChannelDescription>
+                        </ChannelDetails>
+                    </ChannelWrapper>
+                </VideoContainer>
+            </PageWrapper>
         );
     })
 );
